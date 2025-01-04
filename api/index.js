32b37f7e13d24ee1d41164648e822ddeece25a13
tarang5757 <tarang5757@gmail.com>
2025-01-04 14:12:26 -0500

    update

---
+++ api/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import postRoutes from "./routes/posts.js";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+const app = express();
+
+//middleware parses  ubcinubg hsib reqests. it is a generic middleware applied to all incoming requests.
+app.use(express.json());
+
+//This means that all routes defined in postRoutes (which is posts.js) are prefixed with /api/posts.
+app.use("/api/posts", postRoutes);
+app.use("/api/auth", authRoutes);
+
+
+app.listen(5173, () => {
+  console.log("connected!");
+});
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+const Register = () => {
+  const [error, setError] = useState(null);
+
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  //we are making an api request so its async
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/api/auth/register", inputs);
+      navigate("/login");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+
+  return (
+    <div className="auth">
+      <h1>Register</h1>
+      <form>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          onChange={handleChange}
+          name="username"
+        />
+        <input
+          required
+          type="email"
+          name="email"
+          placeholder="email"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Register</button>
+        <p>This is an error!</p>
+        <p>
+          Do you have an account? <Link to="/login">login</Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
